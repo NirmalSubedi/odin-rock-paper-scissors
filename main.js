39a09bb9abacee151a4ed6aafe6498f03ac04ec2
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2025-07-27 15:48:17 -0700

    add pseudocode to main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("Hello World!");
+/* CREATE a function to get the computer choice */
+    // CALCULATE a value to determine the choice
+    // RETURN 'rock', 'paper', or 'scissors' as string 
+    // TEST return value in the console (REMOVE afterwards)
+
+/* CREATE a function to get the user choice */
+    // DETERMINE the value entered by user (USE prompt)
+    // TEST return value in the console (REMOVE afterwards)
+
+/* CREATE two variables to keep track of scores */
+    // INITIALIZE both variables to 0 in the global scope
+
+/* CREATE a function to play a round */
+    // NAME the function playRound
+    // USE humanChoice and computerChoice as parameters to take arguments
+    // MAKE humanChoice parameter case-insensitive
+    // OUTPUT in the console a string to show round winner
+    // INCREASE the winner's score
+
+/* CREATE a function to play the entire game */
+    // NAME the function playGame
+    // MOVE the playRound function into this function
+    // CALL playRound function 5 times
